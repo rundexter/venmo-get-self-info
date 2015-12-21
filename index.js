af5a58af7aeb710d11bc8c1a410f43a9af7ebe9f
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-12-21 19:32:32 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,44 @@
+var _ = require('lodash'),
+    util = require('./util.js');
+
+var request = require('request').defaults({
+    baseUrl: 'https://api.venmo.com/v1/'
+});
+
+var pickInputs = {
+        'user_id': 'userId'
+    },
+    pickOutputs = {
+        'id': 'data.user.id',
+        'username': 'data.user.username',
+        'first_name': 'data.user.first_name',
+        'last_name': 'data.user.last_name',
+        'display_name': 'data.user.display_name',
+        'about': 'data.user.about',
+        'email': 'data.user.email',
+        'phone': 'data.user.phone'
+    };
+
 module.exports = {
+    /**
+     * Process data or error.
+     *
+     * @param error
+     * @param response
+     * @param body
+     */
+    processResult: function (error, response, body) {
+
+        if (error)
+            this.fail(error);
+
+        else if (body.error)
+            this.fail(body.error);
+
+        else
+            this.complete(util.pickResult(body, pickOutputs));
+    },
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +46,22 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var accessToken = dexter.environment('venmo_access_token'),
+            inputs = util.pickStringInputs(step, pickInputs),
+            uriLink = 'me';
+
+        // check params.
+        if (!accessToken)
+            return this.fail('A [venmo_access_token] environment need for this module.');
+
+        //send API request
+        request.get({
+            url: uriLink,
+            qs: _.merge({access_token: accessToken}, inputs),
+            json: true
+        }, function (error, response, body) {
+
+            this.processResult(error, response, body);
+        }.bind(this));
     }
 };
